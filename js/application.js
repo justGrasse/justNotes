5f86332c99cd8f8f78ecd0e7140f53fe6dc1b92f
Justin Grasse <just.grasse@gmail.com>
2017-03-05 14:34:34 -0800

    update colors with jQuery buttons

--- js/application.js
+++ js/application.js
@@ -9,11 +9,18 @@ $(document).ready(function() {
     $("#notes-container").prepend(getNote(noteBody));
   })
 
-function getNote(body){
-  var noteTemplate = '<article class="note shadow rounded" id="note-12"> \
-      <div class="note-body">' + body + '</div> \
-    </article>';
-  return noteTemplate;
-}
+  $('#notes-container').on('click', '#color-buttons button', function() {
+    console.log($(this));
+    var $note = $(this).closest('.note');
+    var color = $(this).data('color');
+    $note.css("background-color", color);
+  })
+
+  function getNote(body){
+    var noteTemplate = '<article class="note shadow rounded" id="note-12"> \
+        <div class="note-body">' + body + '</div> \
+      </article>';
+    return noteTemplate;
+  }
 
 });
